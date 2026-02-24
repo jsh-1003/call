11ed5f16fb0a352cb8f37f5354d2e40cf86e27b9
정선훈 <wjdtjsgns@gmail.com>
2026-02-24 10:08:30 +0900

    일반 배치 수정분 원복(실수로 유료 대신 일반을 수정)

--- adm/call/tmp.php
+++ adm/call/tmp.php
@@ -1,6 +1,10 @@
 <?php
 require_once './_common.php';
+include_once(G5_LIB_PATH.'/call.assign.lib.php');
 
+exit;
+$res = call_assign_pick_and_lock(10, 11, 1, 30, 112233);
+var_dump($res);
 goto_url('./tmp2.php?od_id=1222334');
 exit;
 $mb_no = 436;
